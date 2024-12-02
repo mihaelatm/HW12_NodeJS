8440d5116a64c912416f9dfb781a7ed883b91489
Talmazan Mihaela <talmazanmihaela@gmail.com>
2024-12-02 19:28:04 +0100

    fix: add validation for PUT /products/:id

--- app.js
+++ app.js
@@ -66,6 +66,31 @@ app.get("/products/:id", async (req, res) => {
   }
 });
 
+app.put("/products/:id", async (req, res) => {
+  try {
+    const db = getDb();
+    const productId = req.params.id;
+    const updatedProduct = req.body;
+
+    if (!ObjectId.isValid(productId)) {
+      return res.status(400).json({ error: "Invalid product ID" });
+    }
+
+    const result = await db
+      .collection("products")
+      .updateOne({ _id: new ObjectId(productId) }, { $set: updatedProduct });
+
+    if (result.matchedCount === 0) {
+      return res.status(404).json({ error: "Product not found" });
+    }
+
+    res.status(200).json("Product updated successfully");
+  } catch (error) {
+    console.error("Error updating product:", error);
+    res.status(500).json({ error: "Error updating product" });
+  }
+});
+
 connectToDatabase()
   .then(() => {
     app.listen(port, () => {
