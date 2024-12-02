9d2c10558f1894d154c18a6f1fa3568b25aa0cec
Talmazan Mihaela <talmazanmihaela@gmail.com>
2024-12-02 18:06:18 +0100

    feat: add MongoDB connection setup

---
+++ db/index.js
@@ -0,0 +1,29 @@
+import { MongoClient } from "mongodb";
+import "dotenv/config";
+
+const uri = process.env.MONGO_URI;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+let dbConnection;
+
+async function connectToDatabase() {
+  try {
+    await client.connect();
+    console.log("Connected successfully to MongoDB");
+    dbConnection = client.db();
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error);
+  }
+}
+
+function getDb() {
+  if (!dbConnection) {
+    throw new Error("Database not connected");
+  }
+  return dbConnection;
+}
+
+export { connectToDatabase, getDb };
